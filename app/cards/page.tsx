89cdadd48aa6139d56257d245649cc5665a104f6
Ali Al-Shaikh <mi3lix9@outlook.sa>
2023-05-15 00:18:59 +0300

    feat: cards page

---
+++ app/cards/page.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import { supabase } from "@/utils/supabase";
+import { useEffect, useState } from "react";
+
+export default function CardsPage() {
+  const [cards, setCards] = useState([]);
+  useEffect(() => {
+    (async function () {
+      const { data: cards } = await supabase
+        .from("Card")
+        .select("*, Player(*), Tournament(*)");
+      setCards(cards as any);
+
+      console.log(cards);
+    })();
+  }, []);
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+        <p className="welcome-page">
+          <h1>Cards</h1>
+        </p>
+      </div>
+      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+        <ul>
+          {cards.map((card) => (
+            <li key={`${card.player_id}${card.tournamnet_number}`}>
+              <p
+                className={
+                  (card.color as string).toLocaleLowerCase() === "red"
+                    ? `text-red-500`
+                    : "text-yellow-500"
+                }
+              >
+                Player {card.Player.name} got {card.color} card in{" "}
+                {card.Tournament.name} tournament.
+              </p>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </main>
+  );
+}
